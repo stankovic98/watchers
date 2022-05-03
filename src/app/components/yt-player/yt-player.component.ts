7343e6c43b33671ab38f364231faf685c2602825
Antonio Stankovic <antonio.stankovic77@gmail.com>
2022-05-03 12:34:26 +0200

    working watchtime tracker with dates

--- src/app/components/yt-player/yt-player.component.ts
+++ src/app/components/yt-player/yt-player.component.ts
@@ -6,7 +6,9 @@ import {
   Input,
   OnInit,
   ViewChild,
+  ViewRef,
 } from '@angular/core';
+import { throwIfEmpty } from 'rxjs';
 
 @Component({
   selector: 'app-yt-player',
@@ -14,12 +16,19 @@ import {
   styleUrls: ['./yt-player.component.scss'],
 })
 export class YtPlayerComponent implements AfterViewInit {
-  @ViewChild('youTubePlayer') youTubePlayer: ElementRef<HTMLDivElement>;
+  @ViewChild('youTubePlayerContainer')
+  ytPlayerContainer: ElementRef<HTMLDivElement>;
   customPlayerVars: YT.PlayerVars = {
     modestbranding: 1,
     rel: 0,
   };
 
+  watchTime = {
+    percentageWatched: Array.from({ length: 100 }, (v, i) => -i),
+    start: 0,
+    secondsToPercentage: 0,
+    percentageStart: 0,
+  };
   videoHeight: number | undefined;
   videoWidth: number | undefined;
 
@@ -30,14 +39,53 @@ export class YtPlayerComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.onResize();
     window.addEventListener('resize', this.onResize.bind(this));
+    console.log(this.watchTime.percentageWatched);
   }
 
   onResize(): void {
     this.videoWidth = Math.min(
-      this.youTubePlayer.nativeElement.clientWidth,
+      this.ytPlayerContainer.nativeElement.clientWidth,
       1200
     );
     this.videoHeight = this.videoWidth * 0.6;
     this.changeDetectorRef.detectChanges();
+  }
+
+  initWatchTime(e: YT.PlayerEvent) {
+    this.watchTime.secondsToPercentage = e.target.getDuration() / 100;
+  }
+
+  stateHasChanged(onStateChange: YT.OnStateChangeEvent) {
+    let player = onStateChange.target;
+    if (player.getPlayerState() === YT.PlayerState.PLAYING) {
+      this.watchTime.start = new Date().getTime();
+      this.watchTime.percentageStart =
+        player.getCurrentTime() / player.getDuration();
+    } else {
+      let timePassed = (new Date().getTime() - this.watchTime.start) / 1000;
+      if (timePassed > player.getDuration()) {
+        timePassed = 0;
+      }
+      let percentagePassed = timePassed / player.getDuration();
+      // console.log('time passed: ', timePassed);
+      // console.log('percentage passed: ', percentagePassed);
+      // console.log('percentage start: ', this.watchTime.percentageStart);
+      this.changePercentageOfWatchedVideo(
+        this.watchTime.percentageStart,
+        percentagePassed
+      );
+    }
+
+    console.log('--------------------------------');
+  }
+
+  changePercentageOfWatchedVideo(perStart: number, perDuration: number) {
+    perStart = Math.floor(perStart * 100);
+    perDuration = Math.floor(perDuration * 100);
+    console.log(perStart, perDuration);
+    for (let i = perStart; i < perStart + perDuration; i++) {
+      this.watchTime.percentageWatched[i] *= -1;
+    }
+    console.log(this.watchTime.percentageWatched);
   }
 }
